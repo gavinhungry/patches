639066fce786926cf2e16d8156329ae7cdba3d7a
Gavin Lloyd <gavinhungry@gmail.com>
2012-05-01 13:11:58 -0700

    Thunar 1.4.0

---
+++ thunar/patch.sh
@@ -0,0 +1,7 @@
+for P in ${1}/*.patch; do
+  patch -p0 < $P;
+done
+
+cd ../abs/thunar/src/Thunar-1.4.0/thunar/
+echo exo-csource thunar_location_buttons_ui
+exo-csource --strip-comments --strip-content --static --name=thunar_location_buttons_ui thunar-location-buttons-ui.xml > thunar-location-buttons-ui.h
