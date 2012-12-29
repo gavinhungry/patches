9befc4ed5c23c6140084e437ef22e350df50a10d
Gavin Lloyd <gavinhungry@gmail.com>
2012-12-29 15:54:15 -0800

    Thunar 1.6.2

--- thunar/patch.sh
+++ thunar/patch.sh
@@ -2,7 +2,7 @@ for P in ${1}/*.patch; do
   patch -N -r - -p0 < $P;
 done
 
-cd abs/thunar/src/Thunar-1.6.1/thunar/
+cd abs/thunar/src/Thunar-1.6.2/thunar/
 echo exo-csource
 exo-csource --strip-comments --strip-content --static --name=thunar_location_buttons_ui thunar-location-buttons-ui.xml > thunar-location-buttons-ui.h
 exo-csource --strip-comments --strip-content --static --name=thunar_launcher_ui thunar-launcher-ui.xml > thunar-launcher-ui.h
