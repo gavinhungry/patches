9f27b80303b1e68bffa9ab73f5e926e0200a7bda
Gavin Lloyd <gavinhungry@gmail.com>
2015-05-19 14:35:22 -0700

    Update main scripts, custom package scripts

--- pkgs/thunar/patch.sh
+++ pkgs/thunar/patch.sh
@@ -1,4 +1,7 @@
+ARCH_DIR=$1
+PKGSRC_DIR=$2
+
 inform 'Executing exo-csource'
-cd thunar
+cd ${PKGSRC_DIR}/thunar
 exo-csource --strip-comments --strip-content --static --name=thunar_location_buttons_ui thunar-location-buttons-ui.xml > thunar-location-buttons-ui.h
 exo-csource --strip-comments --strip-content --static --name=thunar_launcher_ui thunar-launcher-ui.xml > thunar-launcher-ui.h
