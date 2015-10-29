b60d389f1bffe54407959866215aaa20192d876d
Gavin Lloyd <gavinhungry@gmail.com>
2015-10-29 08:53:20 -0700

    Ignore packages that are not installed

--- unpatched.sh
+++ unpatched.sh
@@ -8,7 +8,7 @@ for DIR in ${PATCHES_DIR}/* ; do
   PKG=$(basename ${DIR})
   PACKAGER=$(pacman -Qi ${PKG} 2>&1 | grep ^Packager | cut -d':' -f2-)
 
-  if [[ ${PACKAGER} != *"[p]" ]]; then
+  if [[ ${PACKAGER} != *"[p]" ]] && pacman -Q ${PKG} &> /dev/null; then
     arge download && pkgsource ${PKG} || echo ${PKG}
   fi
 done
