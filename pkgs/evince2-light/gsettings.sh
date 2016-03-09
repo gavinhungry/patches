d4b0b4e50eac914034d666e3cac95bd21dfe3047
Gavin Lloyd <gavinhungry@gmail.com>
2016-03-09 14:31:30 -0800

    Add gsettings.sh

---
+++ pkgs/evince2-light/gsettings.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gsettings set org.gnome.Evince.Default show-sidebar false
+gsettings set org.gnome.Evince.Default show-toolbar false
+gsettings set org.gnome.Evince.Default sizing-mode free
+gsettings set org.gnome.Evince.Default window-ratio "(1.0, 1.0)"
