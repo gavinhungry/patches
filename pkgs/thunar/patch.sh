aba43c4c97dd7612c18405a983559800ca7b2a67
Gavin Lloyd <gavinhungry@gmail.com>
2016-05-10 16:17:54 -0700

    Add thunar-create_empty_file_menuitem.patch

--- pkgs/thunar/patch.sh
+++ pkgs/thunar/patch.sh
@@ -3,6 +3,8 @@ PKGSRC_DIR=$2
 
 inform 'Executing exo-csource'
 cd ${PKGSRC_DIR}/thunar
+
+exo-csource --strip-comments --strip-content --static --name=thunar_standard_view_ui thunar-standard-view-ui.xml > thunar-standard-view-ui.h
 exo-csource --strip-comments --strip-content --static --name=thunar_location_buttons_ui thunar-location-buttons-ui.xml > thunar-location-buttons-ui.h
 exo-csource --strip-comments --strip-content --static --name=thunar_launcher_ui thunar-launcher-ui.xml > thunar-launcher-ui.h
 
