ba7064f27c6aba67c8e36c744be9bef554238921
Gavin Lloyd <gavinhungry@gmail.com>
2016-05-11 08:10:34 -0700

    Update evince2

--- pkgs/evince2-light/gsettings.sh
+++ pkgs/evince2-light/gsettings.sh
@@ -2,5 +2,5 @@
 
 gsettings set org.gnome.Evince.Default show-sidebar false
 gsettings set org.gnome.Evince.Default show-toolbar false
-gsettings set org.gnome.Evince.Default sizing-mode free
-gsettings set org.gnome.Evince.Default window-ratio "(1.0, 1.0)"
+gsettings set org.gnome.Evince.Default sizing-mode fit-width
+gsettings set org.gnome.Evince.Default window-ratio '(1.4, 1.0)'
