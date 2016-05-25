e2b7b687b3b559c06825fe24a9fe0053c0a18cbc
Gavin Lloyd <gavinhungry@gmail.com>
2016-05-25 14:20:26 -0700

    Hard-update with no output

--- hard-update.sh
+++ hard-update.sh
@@ -7,9 +7,10 @@ cp -r ${PKGBUILD_DIR} ${PKGBUILD_DIR}.ORIG
 cd ${PATCHER_DIR}/..
 
 inform 'Hard-updating patches to' ${PKGSRC_DIR_BASE}
-for PATCHFILE in ${PATCHES[@]}; do
-  patch -p0 < $PATCHFILE
-  diff -ru ${PKGBUILD_PARENT}/${PKG}.ORIG ${PKGBUILD_PARENT}/${PKG} > ${PATCHFILE}
+for PATCH in ${PATCHES[@]}; do
+  inform 'Updating' $(basename ${PATCH})
+  patch -Ns -r - -p0 < $PATCH
+  diff -ru ${PKGBUILD_PARENT}/${PKG}.ORIG ${PKGBUILD_PARENT}/${PKG} > ${PATCH}
 
   rm -fr ${PKGBUILD_DIR}
   cp -r ${PKGBUILD_DIR}.ORIG ${PKGBUILD_DIR}
