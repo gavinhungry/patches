1f1c8feac82ce51ffbc3447b4666241476b315f7
Gavin Lloyd <gavinhungry@gmail.com>
2016-12-29 10:19:01 -0800

    Use --no-backup-if-mismatch with hard-update

--- hard-update.sh
+++ hard-update.sh
@@ -9,7 +9,7 @@ cd ${PATCHER_DIR}/..
 inform 'Hard-updating patches to' ${PKGSRC_DIR_BASE}
 for PATCH in ${PATCHES[@]}; do
   inform 'Updating' $(basename ${PATCH})
-  patch -Ns -r - -p0 < $PATCH
+  patch -Ns -r - -p0 --no-backup-if-mismatch < $PATCH
   diff -ru ${PKGBUILD_PARENT}/${PKG}.ORIG ${PKGBUILD_PARENT}/${PKG} > ${PATCH}
 
   rm -fr ${PKGBUILD_DIR}
