0698313b323099620ef27df9b4314bd1987f0af8
Gavin Lloyd <gavinhungry@gmail.com>
2017-01-15 23:02:33 -0800

    Add .pkgignore

--- unpatched.sh
+++ unpatched.sh
@@ -8,7 +8,10 @@ for DIR in ${PATCHES_DIR}/* ; do
   PKG=$(basename ${DIR})
   PACKAGER=$(pacman -Qi ${PKG} 2>&1 | grep ^Packager | cut -d':' -f2-)
 
-  if [[ ${PACKAGER} != *"[p]" ]] && pacman -Q ${PKG} &> /dev/null; then
+  if [[ ${PACKAGER} != *"[p]" ]] && \
+    pacman -Q ${PKG} &> /dev/null && \
+    ! grep "^${PKG}$" .pkgignore &> /dev/null;
+  then
     arge download && pkgsource ${PKG} || echo ${PKG}
   fi
 done
