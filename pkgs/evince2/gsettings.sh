3596e4c49d80482c963a8cc1d67ade04af5f7285
Gavin Lloyd <gavinhungry@gmail.com>
2018-05-23 14:45:00 -0700

    Evince -> Evince2

--- pkgs/evince2/gsettings.sh
+++ pkgs/evince2/gsettings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gsettings set org.gnome.Evince.Default show-sidebar false
-gsettings set org.gnome.Evince.Default show-toolbar false
-gsettings set org.gnome.Evince.Default sizing-mode fit-width
-gsettings set org.gnome.Evince.Default window-ratio '(1.4, 1.0)'
+gsettings set org.gnome.Evince2.Default show-sidebar false
+gsettings set org.gnome.Evince2.Default show-toolbar false
+gsettings set org.gnome.Evince2.Default sizing-mode fit-width
+gsettings set org.gnome.Evince2.Default window-ratio '(1.4, 1.0)'
