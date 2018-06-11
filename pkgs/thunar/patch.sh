bab4fc5a23532a96076f36b1de1ef6f556d2a611
Gavin Lloyd <gavinhungry@gmail.com>
2018-06-11 11:42:03 -0700

    Hard-update thunar -> 1.8.0

--- pkgs/thunar/patch.sh
+++ pkgs/thunar/patch.sh
@@ -3,5 +3,4 @@
 inform 'Executing exo-csource'
 cd "$PKGSRC_DIR"/thunar
 exo-csource --strip-comments --strip-content --static --name=thunar_standard_view_ui thunar-standard-view-ui.xml > thunar-standard-view-ui.h
-exo-csource --strip-comments --strip-content --static --name=thunar_location_buttons_ui thunar-location-buttons-ui.xml > thunar-location-buttons-ui.h
 exo-csource --strip-comments --strip-content --static --name=thunar_launcher_ui thunar-launcher-ui.xml > thunar-launcher-ui.h
