7fefc37c3bfa6d95b618148938b53dae2bf7568e
Gavin Lloyd <gavinhungry@gmail.com>
2018-06-15 17:11:59 -0700

    Remove "Send To" from thunar-shortcuts-pane-ui

--- pkgs/thunar/patch.sh
+++ pkgs/thunar/patch.sh
@@ -2,7 +2,8 @@
 
 inform 'Executing exo-csource'
 cd "$PKGSRC_DIR"/thunar
-exo-csource --strip-comments --strip-content --static --name=thunar_launcher_ui thunar-launcher-ui.xml > thunar-launcher-ui.h
-exo-csource --strip-comments --strip-content --static --name=thunar_renamer_dialog_ui thunar-renamer-dialog-ui.xml > thunar-renamer-dialog-ui.h
-exo-csource --strip-comments --strip-content --static --name=thunar_standard_view_ui thunar-standard-view-ui.xml > thunar-standard-view-ui.h
-exo-csource --strip-comments --strip-content --static --name=thunar_window_ui thunar-window-ui.xml > thunar-window-ui.h
+
+# thunar_shortcuts_pane_ui
+for COMPONENT in thunar_launcher_ui thunar_renamer_dialog_ui thunar_standard_view_ui thunar_window_ui; do
+  exo-csource --strip-comments --strip-content --static --name=$COMPONENT ${COMPONENT//_/-}.xml > ${COMPONENT//_/-}.h
+done
