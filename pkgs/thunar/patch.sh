3bfd21ec128b07877568ee3bef622d381719a664
Gavin Lloyd <gavinhungry@gmail.com>
2018-09-26 14:10:47 -0700

    Update thunar -> 1.8.2

--- pkgs/thunar/patch.sh
+++ pkgs/thunar/patch.sh
@@ -3,6 +3,6 @@
 inform 'Executing exo-csource'
 cd "$PKGSRC_DIR"/thunar
 
-for COMPONENT in thunar_launcher_ui thunar_renamer_dialog_ui thunar_shortcuts_pane_ui thunar_standard_view_ui thunar_window_ui; do
+for COMPONENT in thunar_launcher_ui thunar_renamer_dialog_ui thunar_standard_view_ui thunar_window_ui; do
   exo-csource --strip-comments --strip-content --static --name=$COMPONENT ${COMPONENT//_/-}.xml > ${COMPONENT//_/-}.h
 done
