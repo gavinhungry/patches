e7c836b6da3a5a6e08570a8128e15fa91b4295b2
Gavin Lloyd <gavinhungry@gmail.com>
2019-01-09 10:39:03 -0800

    Update virtualbox -> 6.0.0

--- pkgs/virtualbox/patch.sh
+++ pkgs/virtualbox/patch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 inform 'Replacing icons'
-for OSIMG in "$PKGSRC_DIR"/src/VBox/Frontends/VirtualBox/images/{hidpi,}/os_*; do
+for OSIMG in "$PKGSRC_DIR"/src/VBox/Frontends/VirtualBox/images/{x2,x3,x4,}/os_*; do
   cp "$PKGSRC_DIR"/src/VBox/Frontends/VirtualBox/images/OSE/about_16px.png "$OSIMG";
 done
 
