92bd981be4aef91a16b047adae5176e8d53cb0f4
Gavin Lloyd <gavinhungry@gmail.com>
2020-04-08 14:55:15 -0700

    Use rdp.png icon

--- freerdp-git/patch.sh
+++ freerdp-git/patch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 inform 'Executing conv_to_ewm_prop.py'
-"$PKGSRC_DIR"/resources/conv_to_ewm_prop.py ./windows.png "$PKGSRC_DIR"/resources/RDP_Icon.h
-
+"$PKGSRC_DIR"/resources/conv_to_ewm_prop.py ./rdp.png "$PKGSRC_DIR"/resources/RDP_Icon.h
