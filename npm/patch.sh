2102a3dfa8bcd339ad7045c1d82e7334e55a0347
Gavin Lloyd <gavinhungry@gmail.com>
2020-07-08 09:23:40 -0700

    Soft-update npm -> 6.14.6, add npm_no-htmldocs

---
+++ npm/patch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash                                                                                                                                      
+
+inform 'Updating marked/marked-man'
+cd "$PKGSRC_DIR"
+
+npm install marked marked-man
