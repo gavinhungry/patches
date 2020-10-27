05933e87ab035162db2bf3a9616ba3f6d4424343
Gavin Lloyd <gavinhungry@gmail.com>
2020-10-27 09:00:47 -0700

    Update npm patches

--- npm/patch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash                                                                                                                                      
-
-inform 'Updating marked/marked-man'
-cd "$PKGSRC_DIR"
-
-npm install marked marked-man
