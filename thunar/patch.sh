cb88977f5acb781afc71579f543b4aa878502df3
Gavin Lloyd <gavinhungry@gmail.com>
2020-12-26 22:33:47 -0800

    Update thunar -> 4.16.0

--- thunar/patch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-inform 'Executing exo-csource'
-cd "$PKGSRC_DIR"/thunar
-
-for COMPONENT in thunar_launcher_ui thunar_renamer_dialog_ui thunar_standard_view_ui thunar_window_ui; do
-  exo-csource --strip-comments --strip-content --static --name=$COMPONENT ${COMPONENT//_/-}.xml > ${COMPONENT//_/-}.h
-done
