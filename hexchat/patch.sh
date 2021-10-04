efe0f76aa21f1884144bb98f7ca15182eabc7cfb
Gavin Lloyd <gavinhungry@gmail.com>
2021-10-04 08:16:56 -0700

    Update hexchat patch script

--- hexchat/patch.sh
+++ hexchat/patch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+ICONS_DIR="$PKGBUILD_DIR"/src/hexchat/data/icons
+
 inform 'Updating icons'
-cp hexchat.png "$PKGSRC_DIR"/data/icons/hexchat.png
-cp empty.png "$PKGSRC_DIR"/data/icons/tray_fileoffer.png
-cp empty.png "$PKGSRC_DIR"/data/icons/tray_highlight.png
-cp empty.png "$PKGSRC_DIR"/data/icons/tray_message.png
+cp hexchat.png "$ICONS_DIR"/hexchat.png
+cp empty.png "$ICONS_DIR"/tray_fileoffer.png
+cp empty.png "$ICONS_DIR"/tray_highlight.png
+cp empty.png "$ICONS_DIR"/tray_message.png
