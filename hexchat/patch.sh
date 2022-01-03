2b619539e894c7499b672b1e4a61fb0405600c30
Gavin Lloyd <gavinhungry@gmail.com>
2022-01-03 09:26:00 -0800

    Update hexchat icon

--- hexchat/patch.sh
+++ hexchat/patch.sh
@@ -3,7 +3,7 @@
 ICONS_DIR="$PKGBUILD_DIR"/src/hexchat/data/icons
 
 inform 'Updating icons'
-cp hexchat.png "$ICONS_DIR"/hexchat.png
+cp hexchat-indicator.png "$ICONS_DIR"/hexchat.png
 cp empty.png "$ICONS_DIR"/tray_fileoffer.png
 cp empty.png "$ICONS_DIR"/tray_highlight.png
 cp empty.png "$ICONS_DIR"/tray_message.png
