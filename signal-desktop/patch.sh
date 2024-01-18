ca923f4de7767260f7f155260f705bdbc7e91ac1
Gavin Lloyd <gavinhungry@gmail.com>
2024-01-18 12:11:22 -0800

    Update signal-desktop/patch.sh

--- signal-desktop/patch.sh
+++ signal-desktop/patch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 inform 'Replacing icons'
-cp icons/signal-tray.png "$PKGSRC_DIR"/images/icon_256.png
 
-for I in {1..10}; do
-  cp icons/signal-tray-unread.png "$PKGSRC_DIR"/images/alert/256/$I.png
+for PX in 16 32 256; do
+  cp icons/signal-tray.png "$PKGSRC_DIR"/images/icon_$PX.png
+
+  for I in {1..10}; do
+    cp icons/signal-tray-unread.png "$PKGSRC_DIR"/images/alert/$PX/$I.png
+  done
 done
