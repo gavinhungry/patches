df845fde4fcb1b131e7d31d055042005789e573c
Gavin Lloyd <gavinhungry@gmail.com>
2025-10-22 09:59:39 -0700

    Update freerdp

--- freerdp/patch.sh
+++ freerdp/patch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 inform 'Executing conv_to_ewm_prop.py'
-"$PKGSRC_DIR"/resources/conv_to_ewm_prop.py ./rdp.png "$PKGSRC_DIR"/resources/RDP_Icon.h
+"$PKGSRC_DIR"/freerdp/resources/conv_to_ewm_prop.py ./rdp.png "$PKGSRC_DIR"/freerdp/resources/RDP_Icon.h
--- freerdp/pkgsrcdir.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo $PKGBUILD_DIR/$srcdir/$_name-$pkgver
